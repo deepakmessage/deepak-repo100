8be575947f87fb3f6f96a75a7fd31c95c15b83be
deepakmessage <53930367+deepakmessage@users.noreply.github.com>
2023-04-17 07:37:38 -0700

    Update index.php

--- index.php
+++ index.php
@@ -1,56 +1,42 @@
- <?php
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Add Records in Database</title>
+</head>
+<body>
 
- $serverName = "tcp:db-eus-prd.mysql.database.azure.com,3306";
- $connectionOptions = array("Database"=>"mydb",
- "Uid"=>"mysql", "PWD"=>"India@1234567");
- //Establishes the connection
- $conn = sqlsrv_connect($serverName, $connectionOptions);
- //Select Query
- $tsql = "SELECT [CompanyName] FROM SalesLT.Customer";
- //Executes the query
- $getProducts = sqlsrv_query($conn, $tsql);
- //Error handling
- if ($getProducts == FALSE)
- die(FormatErrors(sqlsrv_errors()));
- $productCount = 0;
- $ctr = 0;
- ?> 
- <h1> First 10 results are : </h1>
- <?php
- while($row = sqlsrv_fetch_array($getProducts, SQLSRV_FETCH_ASSOC))
- { 
- if($ctr>9)
- break; 
- $ctr++;
- echo($row['CompanyName']);
- echo("<br/>");
- $productCount++;
- }
- sqlsrv_free_stmt($getProducts);
+<?php
+include "dbConn.php"; // Using database connection file here
 
- $tsql = "INSERT SalesLT.Product (Name, ProductNumber, StandardCost, ListPrice, SellStartDate) OUTPUT INSERTED.ProductID VALUES ('SQL New 1', 'SQL New 2', 0, 0, getdate())";
- //Insert query
- $insertReview = sqlsrv_query($conn, $tsql);
- if($insertReview == FALSE)
- die(FormatErrors( sqlsrv_errors()));
- ?> 
- <h1> Product Key inserted is :</h1> 
- <?php
- while($row = sqlsrv_fetch_array($insertReview, SQLSRV_FETCH_ASSOC))
- { 
- echo($row['ProductID']);
- }
- sqlsrv_free_stmt($insertReview);
- //Delete Query
- //We are deleting the same record
- $tsql = "DELETE FROM [SalesLT].[Product] WHERE Name=?";
- $params = array("SQL New 1");
+if(isset($_POST['submit']))
+{		
+    $fullname = $_POST['fullname'];
+    $age = $_POST['age'];
 
- $deleteReview = sqlsrv_prepare($conn, $tsql, $params);
- if($deleteReview == FALSE)
- die(FormatErrors(sqlsrv_errors()));
+    $insert = mysqli_query($db,"INSERT INTO `tblemp`(`fullname`, `age`) VALUES ('$fullname','$age')");
 
- if(sqlsrv_execute($deleteReview) == FALSE)
- die(FormatErrors(sqlsrv_errors()));
+    if(!$insert)
+    {
+        echo mysqli_error();
+    }
+    else
+    {
+        echo "Records added successfully.";
+    }
+}
 
- ?>
+mysqli_close($db); // Close connection
+?>
+
+<h3>Fill the Form</h3>
+
+<form method="POST">
+  Full Name : <input type="text" name="fullname" placeholder="Enter Full Name" Required>
+  <br/>
+  Age : <input type="text" name="age" placeholder="Enter Age" Required>
+  <br/>
+  <input type="submit" name="submit" value="Submit">
+</form>
+
+</body>
+</html>

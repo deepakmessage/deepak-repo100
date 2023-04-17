ed90d8ba6df11416c8e5a6a35965b4b1500f2ce1
deepakmessage <53930367+deepakmessage@users.noreply.github.com>
2023-04-17 08:13:15 -0700

    Update dbConn.php

--- dbConn.php
+++ dbConn.php
@@ -1,8 +1,13 @@
 <?php
-$conn = mysqli_init();
-$db = mysqli_real_connect("db-eus-prd.mysql.database.azure.com","mysql","India@1234567","mydb", "3306");
+$host = 'db-eus-prd.mysql.database.azure.com';
+$username = 'mysql';
+$password = 'India@1234567';
+$db_name =  'mydb';
 
-if(!$db)
+$conn = mysqli_init();
+mysqli_real_connect($conn, $host, $username, $password, $db_name, 3306);
+
+if(mysql_connect_errno($conn))
 {
     die("Connection failed: " . mysqli_connect_error());
 }

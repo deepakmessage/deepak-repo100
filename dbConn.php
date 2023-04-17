14dc102ba0d37c65c16655acb3dc60b475c72028
deepakmessage <53930367+deepakmessage@users.noreply.github.com>
2023-04-17 07:49:00 -0700

    Update dbConn.php

--- dbConn.php
+++ dbConn.php
@@ -1,6 +1,6 @@
 <?php
 
-$db = mysqli_connect("db-eus-prd.mysql.database.azure.com","mysql","India@1234567","mydqb");
+$db = mysqli_connect("db-eus-prd.mysql.database.azure.com","mysql","India@1234567","mydb");
 
 if(!$db)
 {

225b0655741c5e0ab243de7aec74c03c37e69d0e
deepakmessage <53930367+deepakmessage@users.noreply.github.com>
2023-04-18 01:28:50 -0700

    Update index.php

--- index.php
+++ index.php
@@ -1,42 +1,8 @@
-<!DOCTYPE html>
 <html>
 <head>
-  <title>Add Records in Database</title>
+<title>PHP Test</title>
 </head>
 <body>
-
-<?php
-include "dbConn.php"; // Using database connection file here
-
-if(isset($_POST['submit']))
-{		
-    $fullname = $_POST['fullname'];
-    $age = $_POST['age'];
-
-    $insert = mysqli_query($db,"INSERT INTO `tblemp`(`fullname`, `age`) VALUES ('$fullname','$age')");
-
-    if(!$insert)
-    {
-        echo mysqli_error();
-    }
-    else
-    {
-        echo "Records added successfully.";
-    }
-}
-
-mysqli_close($db); // Close connection
-?>
-
-<h3>Fill the Form</h3>
-
-<form method="POST">
-  Full Name : <input type="text" name="fullname" placeholder="Enter Full Name" Required>
-  <br/>
-  Age : <input type="text" name="age" placeholder="Enter Age" Required>
-  <br/>
-  <input type="submit" name="submit" value="Submit">
-</form>
-
+<?php echo '<p>Hello World</p>'; ?>
 </body>
 </html>
